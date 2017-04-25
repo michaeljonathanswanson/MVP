a2f416ac5f7c9ad4d2014442df04c1d004f054cd
Michael Swasnon <thor.0688@gmail.com>
2017-04-25 16:57:14 -0500

    still a work in progress

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 angular.module('app', [])
-  .controller('infoController', ['$scope', function($scope) {
-    console.log('inside controller', $scope)
+  .controller('infoController', ['$scope', '$http', function($scope, $http) {
     $scope.list = [];
 
     $scope.submit = function() {
-      if ($scope.text) {
-        $scope.list.push(this.text);
-        $scope.text = '';
-        console.log($scope.list);
-      }
+
+      $http.post('http://localhost:8080', console.log({
+        diray: this.sampleText
+      }));
+
+      $http.get('http://localhost:8080')
+          .then(function(response) {
+            console.log(response)
+          });
+
+      // if ($scope.text) {
+      //   $scope.list.push(this.text);
+      //   $scope.text = '';
+      //   console.log($scope.list);
+      // }
     };
+
+
+
   }]);
+
 
   //need to make a get and a post request
--- server.js
+++ server.js
@@ -1,14 +1,43 @@
+var mongoose = require('mongoose');
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mvp');
-
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
 var db = mongoose.connection;
+var diraySchema = mongoose.Schema({
+  diray: String
+});
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/', function(req, res, next) {
+  console.log('POST BODY', req.body);
+  res.send('test');
+  next();
+})
+app.use(serveStatic(__dirname), function(req, res, next) {
+  console.log('req method', req.method);
+  console.log('req params', req.params);
+
+
+  if (req.method === "POST") {
+    console.log('req BODY', req.body)
+    diraySchema.diray = JSON.stringify(req.body)
+  }
+  next();
+})
+// console.log('DB', db);
+
+
+mongoose.connect('mongodb://localhost/mvp');
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('im listining----------------')
+  console.log('DB listining')
+});
+
+
+app.listen(8080, function(){
+  console.log('Server running on 8080...');
 });
